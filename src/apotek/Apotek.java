92373b9ab33447ce4dee9b44418ed5f1974cf2ab
praktek <praktek@192.168.4.17>
2016-11-07 17:26:19 +0700

    tes

---
+++ src/apotek/Apotek.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package apotek;
+
+/**
+ *
+ * @author Rita faria candra
+ */
+public class Apotek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Login main = new Login();
+       String ts[] = {"a","b"};
+       main.main(ts);
+    }
+    
+}
---
+++ src/apotek/JCalendar.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package apotek;
+
+/**
+ *
+ * @author Rita Faria Candra
+ */
+class JCalendar {
+    
+}
